474eb92530cc0430dacb55978e0d6ee3a1dbad9f
santhoshmisanalyst@gmail.com <santhoshmisanalyst@gmail.com>
2023-12-31 15:50:01 +0300

    url comments adeed

--- ToDoApp_app1/urls.py
+++ ToDoApp_app1/urls.py
@@ -1,6 +1,7 @@
 # todo_list/todo_app/urls.py
 from django.urls import path
 from ToDoApp_app1 import views
+#Import othets
 
 urlpatterns = [
     path("", views.ListListView.as_view(), name="index"),
